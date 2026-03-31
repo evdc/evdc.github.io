d31045d713fdf6ecc408d69083e15f2f95ace6b8
Evan Crook <evannc@gmail.com>
2026-03-31 12:46:57 -0700

    Fix playground sourcing

---
+++ playground-src/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'playground-src',
+  base: '/playground/',
+  build: {
+    outDir: '../playground',
+    emptyOutDir: true
+  }
+})
